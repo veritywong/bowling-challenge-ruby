8526bcf14ce8b3589b0b149a152d59dbabecf1d4
Verity <veritywong90@gmail.com>
2023-05-21 14:08:52 +0100

    tests with no strike and strike

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,7 +1,37 @@
 class Bowling
-    def score(number_of_rolls, knocked_down_pins)
-        if number_of_rolls > 1 && knocked_down_pins < 10
-            return knocked_down_pins
+
+    def initialize(io)
+        @io = io
+    end
+
+    
+    def game(frame, current_score)
+        running_total = [current_score]
+
+        @io.puts "number of rolls?"
+        number_of_rolls = @io.gets.to_i
+        @io.puts 'number of knocked down pins?'
+        knocked_down_pins = @io.gets.to_i
+        @io.puts "strike on last frame?"
+        strike_on_last_frame = @io.gets
+
+        if strike_on_last_frame == "yes"
+            running_total << (knocked_down_pins * 2)
+            @io.puts "frame: #{frame}"
+            @io.puts "running total: #{running_total.sum}"
+            
+
+        else 
+            # number_of_rolls > 1 && knocked_down_pins < 10
+            running_total << knocked_down_pins
+            @io.puts "frame: #{frame}"
+            @io.puts "running total: #{running_total.sum}"
+            
         end
     end
+
 end
+
+# game = Bowling.new(Kernel)
+# game.score
+
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -2,14 +2,60 @@ require 'bowling'
 
 RSpec.describe 'bowling' do
 
-    context 'when player never scores a strike or spare' do
-        it 'allows two rolls per frame with total score the sum of these rolls' do
-            bowling = Bowling.new
-            rolls = 2
-            knocked_down_pins = 2
-            score = bowling.score(2, 2)
-            expect(score).to eq 2
-        end
+    context 'first frame with no strike or spare' do
+        it 'does not add any bonus points' do
+            io = double :io
+            
+            expect(io).to receive(:puts).with("number of rolls?")
+            expect(io).to receive(:gets).and_return(2)
+            expect(io).to receive(:puts).with('number of knocked down pins?')
+            expect(io).to receive(:gets).and_return(2)
+            expect(io).to receive(:puts).with('strike on last frame?')
+            expect(io).to receive(:gets).and_return('no')
+            expect(io).to receive(:puts).with('frame: 1')
+            expect(io).to receive(:puts).with('running total: 2')
 
+            bowling = Bowling.new(io)
+            bowling.game(1,0)
+            
+        end
     end
+
+    context 'second frame with no strike or spare' do
+        it 'does not add any bonus points' do
+            io = double :io
+            
+            expect(io).to receive(:puts).with("number of rolls?")
+            expect(io).to receive(:gets).and_return(2)
+            expect(io).to receive(:puts).with('number of knocked down pins?')
+            expect(io).to receive(:gets).and_return(2)
+            expect(io).to receive(:puts).with('strike on last frame?')
+            expect(io).to receive(:gets).and_return('no')
+            expect(io).to receive(:puts).with('frame: 2')
+            expect(io).to receive(:puts).with('running total: 4')
+
+            bowling = Bowling.new(io)
+            bowling.game(2, 2)
+            
+        end
+    end
+
+    context 'when strike' do
+        it 'does doubles score of next frame' do
+            io = double :io
+            
+            expect(io).to receive(:puts).with("number of rolls?")
+            expect(io).to receive(:gets).and_return(2)
+            expect(io).to receive(:puts).with('number of knocked down pins?')
+            expect(io).to receive(:gets).and_return(2)
+            expect(io).to receive(:puts).with('strike on last frame?')
+            expect(io).to receive(:gets).and_return('yes')
+            expect(io).to receive(:puts).with('frame: 2')
+            expect(io).to receive(:puts).with('running total: 14')
+
+            bowling = Bowling.new(io)
+            bowling.game(2, 10)
+        end
+    end
+
 end
