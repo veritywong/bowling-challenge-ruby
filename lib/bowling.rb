c089732542e8249c4ef5cb3565ba4dbda0df89ea
Verity <veritywong90@gmail.com>
2023-05-21 15:40:26 +0100

    scoring refactored into separate methods

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -5,40 +5,68 @@ class Bowling
     end
 
     
-    def game(frame, current_score)
+    def score(frame, last_score)
         @frame = frame.to_i
-        @running_total = [current_score]
+        @running_total = [last_score]
 
-        @io.puts "number of rolls?"
-        number_of_rolls = @io.gets.to_i
         @io.puts 'number of knocked down pins?'
         @knocked_down_pins = @io.gets.to_i
 
-        if @frame > 1
-            @io.puts "previous frame rolls?"
-            previous_frame_rolls = @io.gets.to_i
-            @io.puts "previous frame score?"
-            previous_frame_score = @io.gets.to_i
+        return no_bonus_score
+
+        # if @frame > 1
+        #     @io.puts "number of rolls from last turn?"
+        #     previous_frame_rolls = @io.gets.to_i
+        #     @io.puts "previous frame score?"
+        #     previous_frame_score = @io.gets.to_i
                 
-            if previous_frame_rolls == 1 && previous_frame_score == 10
-                @running_total << (@knocked_down_pins * 3)
-                @io.puts "frame: #{@frame}"
-                @io.puts "running total: #{@running_total.sum}"
+        #     if previous_frame_rolls == 1
+        #         @running_total << (@knocked_down_pins * 3)
+        #         @io.puts "frame: #{@frame}"
+        #         @io.puts "running total: #{@running_total.sum}"
 
-            elsif previous_frame_rolls == 2 && previous_frame_score == 10
-                @io.puts "first roll score?"
-                first_score = @io.gets.to_i
+        #     elsif previous_frame_rolls == 2 && previous_frame_score == 10
+        #         @io.puts "first roll score?"
+        #         first_score = @io.gets.to_i
 
-                @running_total << (@knocked_down_pins + first_score)
-                @io.puts "frame: #{@frame}"
-                @io.puts "running total: #{@running_total.sum}"
-            else
-                no_bonus_score
-            end
-        else
-            no_bonus_score
-        end
+        #         @running_total << (@knocked_down_pins + first_score)
+        #         @io.puts "frame: #{@frame}"
+        #         @io.puts "running total: #{@running_total.sum}"
+        #     else
+        #         no_bonus_score
+        #     end
+        # else
+        #     no_bonus_score
+        # end
 
+    end
+
+    def score_when_last_frame_strike(frame, last_score)
+        @frame = frame.to_i
+        @running_total = [last_score]
+
+        @io.puts 'number of knocked down pins?'
+        @knocked_down_pins = @io.gets.to_i
+
+        @running_total << (@knocked_down_pins * 3)
+        @io.puts "frame: #{@frame}"
+        @io.puts "running total: #{@running_total.sum}"
+    end
+
+
+    def score_when_last_frame_spare(frame, last_score)
+        @frame = frame.to_i
+        @running_total = [last_score]
+
+        @io.puts 'number of knocked down pins?'
+        @knocked_down_pins = @io.gets.to_i
+
+        @io.puts "first roll score?"
+        first_score = @io.gets.to_i
+
+        @running_total << (@knocked_down_pins + first_score)
+        @io.puts "frame: #{@frame}"
+        @io.puts "running total: #{@running_total.sum}"
     end
 
     private
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -6,15 +6,13 @@ RSpec.describe 'bowling' do
         it 'does not add any bonus points' do
             io = double :io
             
-            expect(io).to receive(:puts).with("number of rolls?")
-            expect(io).to receive(:gets).and_return(2)
             expect(io).to receive(:puts).with('number of knocked down pins?')
             expect(io).to receive(:gets).and_return(2)
             expect(io).to receive(:puts).with('frame: 1')
             expect(io).to receive(:puts).with('running total: 2')
 
             bowling = Bowling.new(io)
-            bowling.game(1,0)
+            bowling.score(1,0)
             
         end
     end
@@ -23,42 +21,29 @@ RSpec.describe 'bowling' do
         it 'does not add any bonus points' do
             io = double :io
             
-            expect(io).to receive(:puts).with("number of rolls?")
-            expect(io).to receive(:gets).and_return(2)
             expect(io).to receive(:puts).with('number of knocked down pins?')
             expect(io).to receive(:gets).and_return(2)
-
-            expect(io).to receive(:puts).with("previous frame rolls?")
-            expect(io).to receive(:gets).and_return(2)
-            expect(io).to receive(:puts).with("previous frame score?")
-            expect(io).to receive(:gets).and_return(2)              
-
+        
             expect(io).to receive(:puts).with('frame: 2')
             expect(io).to receive(:puts).with('running total: 4')
 
             bowling = Bowling.new(io)
-            bowling.game(2, 2)
+            bowling.score(2, 2)
             
         end
     end
 
     context 'when strike' do
-        it 'doubles score of next frame' do
+        it 'triples score of next frame' do
             io = double :io
             
-            expect(io).to receive(:puts).with("number of rolls?")
-            expect(io).to receive(:gets).and_return(2)
             expect(io).to receive(:puts).with('number of knocked down pins?')
-            expect(io).to receive(:gets).and_return(2)
-            expect(io).to receive(:puts).with("previous frame rolls?")
-            expect(io).to receive(:gets).and_return(1)
-            expect(io).to receive(:puts).with("previous frame score?")
-            expect(io).to receive(:gets).and_return(10)   
+            expect(io).to receive(:gets).and_return(2)  
             expect(io).to receive(:puts).with('frame: 2')
             expect(io).to receive(:puts).with('running total: 16')
 
             bowling = Bowling.new(io)
-            bowling.game(2, 10)
+            bowling.score_when_last_frame_strike(2, 10)
         end
     end
 
@@ -66,41 +51,29 @@ RSpec.describe 'bowling' do
         it 'doubles score of first bowl of next frame' do
             io = double :io
             
-            expect(io).to receive(:puts).with("number of rolls?")
-            expect(io).to receive(:gets).and_return(2)
             expect(io).to receive(:puts).with('number of knocked down pins?')
             expect(io).to receive(:gets).and_return(2)
-            expect(io).to receive(:puts).with("previous frame rolls?")
-            expect(io).to receive(:gets).and_return(2)
-            expect(io).to receive(:puts).with("previous frame score?")
-            expect(io).to receive(:gets).and_return(10)
             expect(io).to receive(:puts).with("first roll score?")
             expect(io).to receive(:gets).and_return(2)
             expect(io).to receive(:puts).with('frame: 2')
             expect(io).to receive(:puts).with('running total: 14')
 
             bowling = Bowling.new(io)
-            bowling.game(2, 10)
+            bowling.score_when_last_frame_spare(2, 10)
         end
     end
 
     context '10th frame with perfect score' do
         it 'totals to 300' do
             io = double :io
-            
-            expect(io).to receive(:puts).with("number of rolls?")
-            expect(io).to receive(:gets).and_return(1)
+
             expect(io).to receive(:puts).with('number of knocked down pins?')
-            expect(io).to receive(:gets).and_return(10)
-            expect(io).to receive(:puts).with("previous frame rolls?")
-            expect(io).to receive(:gets).and_return(1)
-            expect(io).to receive(:puts).with("previous frame score?")
             expect(io).to receive(:gets).and_return(10)
             expect(io).to receive(:puts).with('frame: 10')
             expect(io).to receive(:puts).with('running total: 300')
   
             bowling = Bowling.new(io)
-            bowling.game(10, 270)
+            bowling.score_when_last_frame_strike(10, 270)
         end
     end
 
